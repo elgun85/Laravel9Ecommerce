21ab5f9cfea6b90fe7ed7a03c2ea994504e833d3
elgun <elgun.huseynov@azeurotel.az>
2023-01-30 08:59:11 +0400

    front

---
+++ resources/views/frontend/index.blade-2.php
@@ -0,0 +1,12 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+<!-- Page content-->
+<div class="container">
+<div class="">
+    @include('frontend.layouts.slider')
+</div>
+
+</div>
+@endsection
+
